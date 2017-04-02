894de5f72b741454df507288a41b6c2273c5e3a7
william wei <1342247033@qq.com>
2017-04-02 23:32:18 +0800

    add Search component

---
+++ src/components/Search.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Input, Row, Col, Select, InputNumber, DatePicker, Button, } from 'antd';
+
+export default class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state={
+      key:null
+    };
+    this.search = this.search.bind(this);
+    this.change = this.change.bind(this);
+  }
+
+  search(){
+    let {onSearch} = this.props;
+    onSearch();
+  }
+
+  change(e){
+    const { value } = e.target;
+    this.setState({key:value});
+  }
+
+  render() {
+    return (
+      <div>
+        <Row>
+          <Col span={20}><Input placeholder="input search text" onChange={this.change} /></Col>
+          <Col span={4}><Button icon="search" onClick={this.search} >Search</Button></Col>
+        </Row>
+      </div>
+    );
+
+  }
+}
+

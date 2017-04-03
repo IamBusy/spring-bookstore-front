7615cb2b2a57bcf87338aa4d9d37c9f7a5517be5
william wei <1342247033@qq.com>
2017-04-03 23:02:22 +0800

    add config file

---
+++ src/config.js
@@ -0,0 +1,10 @@
+/**
+ * Created by william on 03/04/2017.
+ */
+
+export default {
+  api:    '//localhost:8080/api',
+  version:'0.0.1',
+  author: 'william wei',
+  email:  'wxl1342247033@gamil.com'
+}

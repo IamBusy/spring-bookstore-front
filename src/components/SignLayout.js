05aa8bc4fbb7144cd02ee8167e1cd1871848f2c2
william wei <1342247033@qq.com>
2017-04-05 00:57:16 +0800

    add particles in background of signLayout

--- src/components/SignLayout.js
+++ src/components/SignLayout.js
@@ -3,33 +3,57 @@
  */
 import {Row, Col, Menu, Icon} from 'antd';
 import {Link} from 'dva/router';
+import React, { Component } from 'react';
+import Particles from 'react-particles-js';
 
 
-export default function SignLayout(props) {
-  return (
-    <div >
-      <Row type="flex" justify="center">
-        <Col>
-          <Row style={{textAlign:'center',fontSize:36,fontFamily:'Microsoft YaHei',color:'#108ee9',paddingTop:80}}>
-            <Icon type="book" />BookStore
-          </Row>
-          <Row style={{paddingTop:20}}>
-            <h2 style={{textAlign:'center'}}>与世界分享你的知识、经验和见解</h2>
-          </Row>
-          <Row type="flex" justify="center">
-            <Menu  mode="horizontal">
-              <Menu.Item key="signin">
-                <Link to='/signin'>Signin</Link>
-              </Menu.Item>
-              <Menu.Item key="signup"><Link to='/signup'>Signup</Link></Menu.Item>
-            </Menu>
-          </Row>
-          <Row style={{paddingTop:20}}>
-            {props.children}
-          </Row>
-        </Col>
-      </Row>
 
-    </div>
-  );
+export default class SignLayout extends Component{
+  constructor(props){
+    super(props);
+
+  }
+
+  render(){
+    return (
+      <div >
+        <Particles width="100%" height="100%" params={{
+          particles: {
+            line_linked: {
+              shadow: {
+                enable: true,
+                color: "#3CA9D1",
+                blur: 5
+              }
+            }
+          }
+        }} style={{position: 'absolute'}}/>
+
+        <Row type="flex" justify="center" style={{zIndex:99}}>
+          <Col>
+            <Row style={{textAlign:'center',fontSize:36,fontFamily:'Microsoft YaHei',color:'#108ee9',paddingTop:80}}>
+              <Icon type="book" />BookStore
+            </Row>
+            <Row style={{paddingTop:20}}>
+              <h2 style={{textAlign:'center'}}>与世界分享你的知识、经验和见解</h2>
+            </Row>
+            <Row type="flex" justify="center">
+              <Menu  mode="horizontal">
+                <Menu.Item key="signin">
+                  <Link to='/signin'>Signin</Link>
+                </Menu.Item>
+                <Menu.Item key="signup"><Link to='/signup'>Signup</Link></Menu.Item>
+              </Menu>
+            </Row>
+            <Row style={{paddingTop:20}}>
+              {this.props.children}
+            </Row>
+          </Col>
+        </Row>
+
+      </div>
+    );
+  }
 }
+
+

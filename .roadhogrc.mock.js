a0bbc382316cfb6f79834b1e0d9c74c01845bd4c
william wei <1342247033@qq.com>
2017-04-04 10:17:52 +0800

    add mock data for category

--- .roadhogrc.mock.js
+++ .roadhogrc.mock.js
@@ -1,3 +1,11 @@
+const Mock = require('mockjs');
+const Random = Mock.Random;
 
 export default {
+  'GET /api/categories':Mock.mock({
+    'data|10':[{
+      'id|+1' : 1,
+      'name'  : Random.ctitle(2,4)
+    }]
+  }),
 };

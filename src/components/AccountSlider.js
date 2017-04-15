6c4dcb8fc1cb5bec6cbc1a22e6ec981ac7562434
william wei <1342247033@qq.com>
2017-04-15 13:44:47 +0800

    add AccountSlider component

---
+++ src/components/AccountSlider.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'dva/router';
+import { Row, Col, Layout, Menu, Icon } from 'antd';
+
+function AccountSlider(props) {
+  const { selectKey } = props;
+  return (
+    <div>
+      <Menu
+        style={{ width: '100%' }}
+        defaultSelectedKeys={[selectKey]}
+        defaultOpenKeys={['sub1']}
+        mode="inline"
+      >
+        <Menu.Item key="information"><Link to="/account">个人信息</Link></Menu.Item>
+        <Menu.Item key="order"><Link to="/order">我的订单</Link></Menu.Item>
+        <Menu.Item key="setting"><Link to="/">设置中心</Link></Menu.Item>
+
+      </Menu>
+
+    </div>
+  );
+}
+
+export default AccountSlider;

9c050f23d219fd32c37f1d473fbc09d603b5dd62
william wei <1342247033@qq.com>
2017-04-15 15:23:25 +0800

    import orders' mock data

--- .roadhogrc.mock.js
+++ .roadhogrc.mock.js
@@ -1,9 +1,11 @@
 import category from './mock/category';
 import product from './mock/product';
 import user from './mock/user';
+import order from './mock/order';
 
 export default {
   ...category,
   ...product.api,
   ...user,
+  ...order,
 };
